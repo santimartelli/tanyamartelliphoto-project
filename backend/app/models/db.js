05464a56a7962ff23f4cc7f4c38c3f409e533992
Santi <santimartelli@gmail.com>
2024-02-14 11:35:03 +0100

    agregado pool para reconectar a la base de datos

--- backend/app/models/db.js
+++ backend/app/models/db.js
@@ -17,21 +17,35 @@ const mysql = require("mysql2");
  * @property {string} password - La contraseña del usuario de la base de datos.
  * @property {string} database - El nombre de la base de datos.
  */
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+connection.getConnection((err, conn) => {
+  if (err) throw err;
+  console.log("Conexión con la base de datos correcta. Id: " + conn.threadId);
+  conn.release(); // Release the connection back to the pool
+});
+
+module.exports = connection;
+
+
+
 
 /**
  * Establece la conexión a la base de datos.
  * @param {function} error - El callback que maneja el error.
  * @memberof db
  */
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
+// connection.connect((error) => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the database.");
+// });
 
-module.exports = connection;
+// module.exports = connection;
